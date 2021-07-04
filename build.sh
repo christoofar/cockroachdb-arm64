75911ffa6bcc024b7c038a77664da465fa25e195
christoofar <christoofar@gmail.com>
2021-07-04 13:31:58 +0100

    updated to 20.2.12

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-docker build -t christoofar/cockroachdb-arm64:20.2.11 -t christoofar/cockroachdb-arm64:latest .
+docker build -t christoofar/cockroachdb-arm64:20.2.12 -t christoofar/cockroachdb-arm64:latest .
